5aed60ac4cddeae65045ec19f7becdbc7fa43f15
Ken Ang <angkendrick@gmail.com>
2015-07-07 18:56:02 +0000

    test_05.rb complete

--- lib/barracks.rb
+++ lib/barracks.rb
@@ -1,6 +1,24 @@
+require_relative 'footman'
+
 class Barracks
 
-  def can_train_footman?
-    true
+  attr_accessor :gold, :food
+
+  def initialize()
+    @gold = 1000
+    @food = 80
   end
+
+  def can_train_footman?
+    self.gold >= 135 && self.food >= 2
+  end
+
+  def train_footman
+    if can_train_footman?
+      @gold -= 135
+      @food -= 2
+      footman = Footman.new()
+    end
+  end
+
 end
---
+++ lib/unit.rb
@@ -0,0 +1,10 @@
+class Unit
+
+  attr_reader :health_points
+
+ def initialize(hp, ap)
+  @health_points = hp
+  @attack_points = ap
+ end
+
+end
